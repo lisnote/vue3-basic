786eec6c273703ce10c142e17785355c27870c1c
lisnote <didongxiaoli@gmail.com>
2022-11-02 18:06:04 +0800

    不使用的变量和参数必须以 _ 作为变量名开头

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true,
-  },
+  env: { browser: true, node: true },
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
     '@vue/typescript/recommended',
     'prettier',
   ],
-  parserOptions: {
-    ecmaVersion: 2021,
-  },
+  parserOptions: { ecmaVersion: 2021 },
   plugins: [],
+  root: true,
   rules: {
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    ],
     'spaced-comment': ['error', 'always', { markers: ['/'] }],
   },
 };
