9aaf3b8fd100752fc43dad2de84311752549a6fe
lisnote <didongxiaoli@gmail.com>
2022-11-02 17:37:52 +0800

    eslint基础配置

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'prettier',
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  plugins: [],
+  rules: {},
+};
