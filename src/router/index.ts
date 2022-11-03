623f9ddef79dfecb2a8651635348e3a7a3c3a4ab
lisnote <didongxiaoli@gmail.com>
2022-11-03 10:51:33 +0800

    feat: 拓展 RouteRecordRow 类型声明

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,22 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
+
+const menuRouters: RouteRecordRaw[] = [
+  {
+    path: '/',
+    title: '首页',
+    component: () => import('@/views/Home'),
+  },
+  {
+    path: '/about',
+    title: '关于',
+    component: () => import('@/views/About'),
+  },
+];
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: [
-    {
-      path: '/',
-      component: () => import('@/views/Home'),
-    },
-    {
-      path: '/about',
-      component: () => import('@/views/About'),
-    },
-  ],
+  routes: menuRouters,
 });
 
-export default router;
+export { router as default, menuRouters };
