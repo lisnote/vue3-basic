20b4add590146fa887b3f42b0db0553e2a543bdb
lisnote <didongxiaoli@gmail.com>
2022-11-03 11:54:48 +0800

    perf: 完善 LisMenu 类型声明, 工具类

--- src/components/LisUI/LisMenu/index.ts
+++ src/components/LisUI/LisMenu/index.ts
@@ -1,2 +1,25 @@
 import index from './index.vue';
+import { cloneDeepWith } from 'lodash-es';
+import { isObject } from '@/utils/types';
+
+import type { RouteRecordRaw } from 'vue-router';
+
 export default index;
+
+export interface LisMenuData {
+  index: string;
+  title: string;
+  children: LisMenuData;
+}
+
+export function MenuRouteAdaptor(data: RouteRecordRaw[]): LisMenuData[] {
+  return cloneDeepWith(data, (value) => {
+    if (isObject(value)) {
+      return {
+        index: value.path,
+        title: value.title,
+        children: MenuRouteAdaptor(value.children),
+      };
+    }
+  });
+}
