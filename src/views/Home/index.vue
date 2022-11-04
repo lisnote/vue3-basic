470b88d93b9782d1fa34e428188e31e31742ffb0
lisnote <didongxiaoli@gmail.com>
2022-11-04 13:38:02 +0800

    feat: 完善首页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,1 +1,14 @@
-<template>Home</template>
+<script lang="ts" setup>
+import pkg from '@root/package.json';
+</script>
+<template>
+  <h2>一个基本的 Vue3 项目</h2>
+  <hr />
+  <div v-for="(key, value) in pkg.dependencies" :key="key">
+    {{ value }} : {{ key }}
+  </div>
+  <hr />
+  <div v-for="(key, value) in pkg.devDependencies" :key="key">
+    {{ value }} : {{ key }}
+  </div>
+</template>
