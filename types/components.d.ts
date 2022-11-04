d09657fd6e6ab2550e9f048e97297aff85ae69df
lisnote <didongxiaoli@gmail.com>
2022-11-04 17:05:04 +0800

    feat: 增加全局 ElInput 组件声明

--- types/components.d.ts
+++ types/components.d.ts
@@ -2,5 +2,7 @@ import LisMenu from '../src/components/LisUI/LisMenu';
 declare module 'vue' {
   export interface GlobalComponents {
     LisMenu: typeof LisMenu;
+
+    ElInput: typeof import('element-plus')['ElInput'];
   }
 }
