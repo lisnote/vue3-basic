249b81de539e9132c60ac247ac2e8a55e5c196c1
lisnote <didongxiaoli@gmail.com>
2022-11-07 21:18:43 +0800

    feat: 首页增加仓库收藏数和更新日期

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,11 +1,23 @@
 <script lang="ts" setup>
 import pkg from '@root/package.json';
 import useMainStore from '@/store';
+import http from '@/utils/http';
+import { ref } from 'vue';
+
+import type { Ref } from 'vue';
+
+const data: Ref<Record<string, string>> = ref({});
+http
+  .get('api/repos/lisnote/vue3-basic')
+  .then(({ data: { stargazers_count, updated_at } }) => {
+    data.value = { stargazers_count, updated_at };
+  });
 </script>
 <template>
   <div v-for="(key, value) in useMainStore().$state" :key="key">
     {{ value }} : {{ key }}
   </div>
+  <div v-for="(key, value) in data" :key="key">{{ value }} : {{ key }}</div>
   <hr />
   <div v-for="(key, value) in pkg.dependencies" :key="key">
     {{ value }} : {{ key }}
