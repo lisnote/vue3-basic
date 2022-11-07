d84336037210db106704475d4fb5459f152b73f6
lisnote <didongxiaoli@gmail.com>
2022-11-07 14:00:43 +0800

    feat: 完善 mock 功能

--- mock/index.ts
+++ mock/index.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import mocker from './mocker';
 
 function startMocker(port: string) {
   const app = express();
-  app.get('*', (req, res, next) => res.end('mocker success'));
+  app.use(mocker());
   app.listen(port, () =>
-    console.log(`mocker started in http://localhost:${port}`),
+    console.log(`mocker started in \x1b[1;36mhttp://localhost:${port}\x1b[0m`),
   );
 }
 
---
+++ mock/mocker.ts
@@ -0,0 +1,36 @@
+import { stat } from 'fs/promises';
+import { readdirSync, readFileSync } from 'fs';
+import { join } from 'path';
+import json5 from 'json5';
+
+import type { Request, Response, NextFunction } from 'express';
+
+const data = {};
+export default function mocker() {
+  dataLoader(join(__dirname, 'data'));
+  return function (req: Request, resp: Response, next: NextFunction) {
+    if (data[req.method] && data[req.method][req.path]) {
+      console.log('mocker access', req.method, req.path);
+      resp.send(data[req.method][req.path]);
+    }
+    next();
+  };
+}
+
+// 加载 mock 数据到 data 对象
+async function dataLoader(dir) {
+  readdirSync(dir).forEach(async (file) => {
+    const fullPath = join(dir, file);
+    stat(fullPath).then((state) => {
+      if (state.isDirectory()) {
+        dataLoader(fullPath);
+      } else if (/\.json5?$/.test(fullPath)) {
+        const jsonData = json5.parse(readFileSync(fullPath).toString());
+        Object.entries(jsonData).forEach(([key, value]) => {
+          data[key] = data[key] ?? {};
+          Object.assign(data[key], value);
+        });
+      }
+    });
+  });
+}
