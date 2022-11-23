dce89da057da06146fb60178ef6e6bf1dcf27a76
lisnote <didongxiaoli@gmail.com>
2022-11-24 00:37:49 +0800

    docs: 完善 LisMenu 组件使用说明

--- src/components/LisUI/LisMenu/index.ts
+++ src/components/LisUI/LisMenu/index.ts
@@ -1,3 +1,8 @@
+/**
+ * 菜单组件
+ * 属性 data 接收一个 LisMenuData 类型的数据
+ * 生成菜单栏, 兼容 el-menu 属性
+ */
 import index from './index.vue';
 import { cloneDeepWith } from 'lodash-es';
 import { isObject } from '@/utils/types';
@@ -14,6 +19,7 @@ export interface LisMenuData {
   onClick?(...args: unknown[]): unknown;
 }
 
+// 将 RouteRecordRow[] 类型的数据转换为 LisMenuData[] 类型的数据
 export function MenuRouteAdaptor(data: RouteRecordRaw[]): LisMenuData[] {
   return cloneDeepWith(data, (value) => {
     if (isObject(value)) {
