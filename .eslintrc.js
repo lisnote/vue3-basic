dcaf76b82cceea65330e65ad388a0f653f3b8c27
lisnote <didongxiaoli@gmail.com>
2022-11-23 12:03:07 +0800

    feat: exlint 允许 any 类型

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   plugins: [],
   root: true,
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
