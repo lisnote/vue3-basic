606085bc0fccaed0317140132b2419d742e8b539
lisnote <didongxiaoli@gmail.com>
2022-11-23 21:52:04 +0800

    feat: 无论什么情况都自动解析blob为文本

--- src/utils/http/response/blobToText.ts
+++ src/utils/http/response/blobToText.ts
@@ -2,10 +2,10 @@ import type { AxiosResponse } from 'axios';
 
 export default async function (res: AxiosResponse) {
   const data: Blob = res.data;
-  if (data.type === 'text/html') {
-    res.data = await data.text();
-  } else if (data.type === 'application/json') {
+  if (data.type === 'application/json') {
     res.data = JSON.parse(await data.text());
+  } else {
+    res.data = await data.text();
   }
   return res;
 }
