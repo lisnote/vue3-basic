8a4ce67ce08567fcfc6abf71e5be3c8ed3c4c90f
lisnote <didongxiaoli@gmail.com>
2022-12-14 18:43:07 +0800

    chore: 移除无意义空行

--- types/components.d.ts
+++ types/components.d.ts
@@ -2,7 +2,6 @@ import LisMenu from '../src/components/LisUI/LisMenu';
 declare module 'vue' {
   export interface GlobalComponents {
     LisMenu: typeof LisMenu;
-
     ElInput: typeof import('element-plus')['ElInput'];
   }
 }
