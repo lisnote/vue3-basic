1315faa929e43d7d68a49420e101c85a5abb73ac
lisnote <didongxiaoli@gmail.com>
2023-04-10 14:52:16 +0800

    fix: jsdoc 无法识别 @typedef

--- src/utils/dataProcessor/tree.ts
+++ src/utils/dataProcessor/tree.ts
@@ -2,24 +2,23 @@
  * 树结构的数据处理方法集
  */
 import { cloneDeep } from 'lodash-es';
-/**
- * @typedef treeCleanerProps
- * @type {object}
- * @prop {string} children
- * @prop {string} filed
- */
+
+interface TreeCleanerProps {
+  children?: string;
+  field?: string;
+}
 
 /**
  * 树结构数据清理, 直接影响原对象, 移除自身及子树指定字段的值不含search值的树
  * @param {any} tree 待处理的树数据
  * @param {string} search 查找的值
- * @param {treeCleanerProps} props 树属性及查找字段
+ * @param {TreeCleanerProps} props 树属性及查找字段
  * @returns {any} 被清理的树
  */
 export function treeCleaner(
   tree: any,
   search: string,
-  props?: { children?: string; field?: string },
+  props?: TreeCleanerProps,
 ): any {
   const { children, field } = Object.assign(
     { children: 'children', field: 'id' },
@@ -38,13 +37,13 @@ export function treeCleaner(
  * 树结构数据过滤, 不影响原对象, 过滤自身及子树指定字段的值不含search值的树
  * @param {any} tree 待处理的树数据
  * @param {string} search 查找的值
- * @param {treeCleanerProps} props 树属性及查找字段
+ * @param {TreeCleanerProps} props 树属性及查找字段
  * @returns {any} 过滤后的树
  */
 export function treeFilter(
   tree: any,
   search: string,
-  props?: { children?: string; field?: string },
+  props?: TreeCleanerProps,
 ): any {
   return treeCleaner(cloneDeep(tree), search, props);
 }
