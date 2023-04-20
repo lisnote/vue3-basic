cc8414742ea325f4578e672888f693db0ec98da7
lisnote <didongxiaoli@gmail.com>
2023-04-20 09:56:05 +0800

    chore: 删除无用注释

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,7 +1,5 @@
-// import RecursiveMenu from '../src/components/RecursiveMenu';
 declare module 'vue' {
   export interface GlobalComponents {
-    // RecursiveMenu: typeof RecursiveMenu;
     ElInput: typeof import('element-plus')['ElInput'];
   }
 }
