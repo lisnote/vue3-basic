75b2348fc92d1cb41ff50455c08ae1b9eadf5b1a
lisnote <didongxiaoli@gmail.com>
2023-05-12 13:04:24 +0800

    feat: 增加获取树节点列表的方法

--- src/utils/dataProcessor/tree.ts
+++ src/utils/dataProcessor/tree.ts
@@ -3,7 +3,7 @@
  */
 import { cloneDeep } from 'lodash-es';
 
-interface TreeCleanerProps {
+interface TreeProps {
   children?: string;
   field?: string;
 }
@@ -12,13 +12,13 @@ interface TreeCleanerProps {
  * 树结构数据修剪, 直接影响原对象, 移除自身及子树指定字段的值不含search值的树
  * @param {T} tree 待处理的树数据
  * @param {string} search 查找的值
- * @param {TreeCleanerProps} props 树属性及查找字段
+ * @param {TreeProps} props 树属性及查找字段
  * @returns {T} 被修剪的树
  */
 export function treeCleaner<T extends any[]>(
   tree: T,
   search: string,
-  props?: TreeCleanerProps,
+  props?: TreeProps,
 ): T {
   const { children, field } = Object.assign(
     { children: 'children', field: 'id' },
@@ -37,13 +37,32 @@ export function treeCleaner<T extends any[]>(
  * 树结构数据过滤, 不影响原对象, 过滤自身及子树指定字段的值不含search值的树
  * @param {T} tree 待处理的树数据
  * @param {string} search 查找的值
- * @param {TreeCleanerProps} props 树属性及查找字段
+ * @param {TreeProps} props 树属性及查找字段
  * @returns {T} 被过滤的树
  */
 export function treeFilter<T extends any[]>(
   tree: T,
   search: string,
-  props?: TreeCleanerProps,
+  props?: TreeProps,
 ): T {
   return treeCleaner(cloneDeep(tree), search, props);
+}
+
+/**
+ * 获取一棵树的节点列表
+ * @param {T} tree 待处理的树
+ * @param {string} children 子节点列表的字段名
+ * @param {T} list 树节点列表
+ * @returns
+ */
+export function treeToList<T extends any[]>(
+  tree: T,
+  children: string = 'children',
+  list: T[number] = [],
+): T {
+  tree.forEach((item) => {
+    list.push(item);
+    if (item[children]?.length) treeToList(item[children], children, list);
+  });
+  return list;
 }
