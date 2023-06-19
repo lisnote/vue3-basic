b76e86b6bc39e177e31200321d5e3779555389f5
lisnote <didongxiaoli@gmail.com>
2023-06-19 14:33:39 +0800

    feat: 移除自定义 mock

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,10 @@ import { ConfigEnv, UserConfigExport, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
 import { resolve } from 'path';
-import mock from './mock';
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfigExport => {
   const env = loadEnv(mode, __dirname, '');
-  mock(mode, env);
   return {
     base: './',
     plugins: [vue(), WindiCSS()],
@@ -18,5 +16,8 @@ export default ({ mode }: ConfigEnv): UserConfigExport => {
       },
     },
     envPrefix: 'APP_',
+    server: {
+      port: Number(env.APP_PORT),
+    },
   };
 };
