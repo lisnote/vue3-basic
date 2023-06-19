ee3606fce0e2169608e163af270ddcf8400f3033
lisnote <didongxiaoli@gmail.com>
2023-06-19 12:00:31 +0800

    feat: el 组件本地化

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+import { ElConfigProvider } from 'element-plus';
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+</script>
 <template>
-  <RouterView />
+  <ElConfigProvider :locale="zhCn">
+    <RouterView />
+  </ElConfigProvider>
 </template>
