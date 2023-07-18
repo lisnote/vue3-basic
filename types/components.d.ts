19fad430b2c78e898bf854adf2b0c58e4de53de3
lisnote <didongxiaoli@gmail.com>
2023-07-18 17:39:34 +0800

    feat: 增加Element全局组件

--- types/components.d.ts
+++ types/components.d.ts
@@ -1,6 +1,7 @@
 declare module 'vue' {
   export interface GlobalComponents {
     ElInput: typeof import('element-plus')['ElInput'];
+    ElScrollbar: typeof import('element-plus')['ElScrollbar'];
   }
 }
 export {};
