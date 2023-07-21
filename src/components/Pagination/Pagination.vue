fa0608eb8371afed7b1b29d4bd445df4d117fca4
lisnote <didongxiaoli@gmail.com>
2023-07-21 10:29:22 +0800

    feat: 封装翻页组件

---
+++ src/components/Pagination/Pagination.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import { ElPagination } from 'element-plus';
+const emit = defineEmits(['change', 'update:page', 'update:limit']);
+const props = defineProps({
+  page: {
+    type: Number,
+    default: () => 1,
+  },
+  limit: {
+    type: Number,
+    default: () => 10,
+  },
+  total: {
+    type: Number,
+    required: true,
+  },
+});
+let page = 1;
+let limit = 10;
+function updatePage(value: number) {
+  page = value;
+  emit('update:page', value);
+  emit('change', page, limit);
+}
+function updateLimit(value: number) {
+  limit = value;
+  emit('update:limit', value);
+  emit('change', page, limit);
+}
+</script>
+
+<template>
+  <ElPagination
+    :bind="$attrs"
+    :total="props.total"
+    :pager-count="5"
+    :current-page="props.page"
+    :page-size="props.limit"
+    layout="total, sizes, prev, pager, next, jumper"
+    background
+    @update:current-page="updatePage"
+    @update:page-size="updateLimit"
+  />
+</template>
