b62f4719345e6a6788bd1ae4a84482ec25a1dac2
lisnote <1292580280@qq.com>
2023-09-11 02:06:34 +0800

    feat: 完善接口类型

--- src/api/basic.ts
+++ src/api/basic.ts
@@ -2,8 +2,16 @@ import http from '@/utils/http';
 import { AxiosRequestConfig } from 'axios';
 
 // 获取验证码
+export const smsCodeType = {
+  signup: 0,
+  login: 1,
+  resetPassword: 2,
+  permanentlyDeleteAccount: 3,
+  twoFactorAuthentication: 4,
+  deleteProject: 5,
+} as const;
 export async function sendSmsCode(
-  data: { name: string; password: string },
+  data: { name: string; type: typeof smsCodeType[keyof typeof smsCodeType] },
   config?: AxiosRequestConfig,
 ) {
   return http.post('/basic/sendSmsCode', data, config);
