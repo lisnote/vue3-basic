2cd522c5a81f97995a4d9c6f80bd17c98c62635f
lisnote <1292580280@qq.com>
2023-09-10 22:21:38 +0800

    perf: 输入提示变更

--- src/views/Login/LoginForm.vue
+++ src/views/Login/LoginForm.vue
@@ -32,7 +32,7 @@ async function submit() {
   <div class="w-full">
     <ElForm ref="formRef" :model="formData" :rules="formRules">
       <ElFormItem prop="name">
-        <ElInput v-model="formData.name" placeholder="请输入账户名" />
+        <ElInput v-model="formData.name" placeholder="请输入用户名" />
       </ElFormItem>
       <ElFormItem prop="password">
         <ElInput
