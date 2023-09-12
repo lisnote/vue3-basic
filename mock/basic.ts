cf17cd8f336ac1dac3bbe5b1c6aed49afeceb8a3
lisnote <didongxiaoli@gmail.com>
2023-09-12 11:52:01 +0800

    feat: mock 增加数据判断

--- mock/basic.ts
+++ mock/basic.ts
@@ -2,7 +2,20 @@ import { createMockMethod } from '.';
 
 export default createMockMethod({
   url: '/basic/sendSmsCode',
-  response: {
-    code: 0,
+  // name 不为 admin 时抛错: 用户不存在
+  rawResponse(req, res) {
+    let body = '';
+    req.on('data', (chunk) => {
+      body += chunk.toString();
+    });
+    req.on('end', () => {
+      const data = JSON.parse(body);
+      if (data.name !== 'admin') {
+        res.statusCode = 404;
+        res.end(JSON.stringify({ code: 40002, message: 'User not found.' }));
+      } else {
+        res.end(JSON.stringify({ code: 0 }));
+      }
+    });
   },
 });
