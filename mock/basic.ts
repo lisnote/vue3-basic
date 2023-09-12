e7704d1864b8d39be803557611d7e5d6c548702b
lisnote <1292580280@qq.com>
2023-09-13 01:51:36 +0800

    feat: 调整 mock 数据

--- mock/basic.ts
+++ mock/basic.ts
@@ -2,21 +2,5 @@ import { createMockMethod } from '.';
 
 export default createMockMethod({
   url: '/basic/sendSmsCode',
-  // phone 不为 18888888888 时抛错: 用户不存在
-  rawResponse(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    let body = '';
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      const data = JSON.parse(body || '{}');
-      if (data.phone !== '18888888888') {
-        res.statusCode = 404;
-        res.end(JSON.stringify({ code: 40002, message: 'User not found.' }));
-      } else {
-        res.end(JSON.stringify({ code: 0 }));
-      }
-    });
-  },
+  response: () => ({ code: 0 }),
 });
--- mock/user.ts
+++ mock/user.ts
@@ -3,32 +3,48 @@ import { createMockMethod } from '.';
 export default createMockMethod(
   {
     url: '/user/login',
-    response: {
-      code: 0,
-      data: {
-        id: '123456',
-        name: 'admin',
-        token: '123456',
-        avatar: 'https://avatars.githubusercontent.com/lisnote',
-        permissionList: [
-          'permissionAdd',
-          'permissionSelect',
-          'permissionEdit',
-          'permissionDelete',
-        ],
-      },
+    rawResponse(this, req, res) {
+      res.setHeader('Content-Type', 'application/json');
+      this.parseJson().then(({ phone, password }) => {
+        if (phone != '18888888888') {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ code: 40002 }));
+        } else if (password != '0192023a7bbd73250516f069df18b500') {
+          res.statusCode = 400;
+          res.end(JSON.stringify({ code: 40003 }));
+        } else {
+          res.end(
+            JSON.stringify({
+              code: 0,
+              data: {
+                id: '123456',
+                name: 'admin',
+                phone: '18888888888',
+                token: '123456',
+                avatar: 'https://avatars.githubusercontent.com/lisnote',
+                permissionList: [
+                  'permissionAdd',
+                  'permissionSelect',
+                  'permissionEdit',
+                  'permissionDelete',
+                ],
+              },
+            }),
+          );
+        }
+      });
     },
+  },
+  {
+    url: '/user/signup',
+    response: () => ({ code: 0 }),
   },
   {
     url: '/user/resetPassword',
-    response: {
-      code: 0,
-    },
+    response: () => ({ code: 0 }),
   },
   {
     url: '/user/logout',
-    response: {
-      code: 0,
-    },
+    response: () => ({ code: 0 }),
   },
 );
