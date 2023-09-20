e9482fe5af9752a8c3b9caa26bfff5fa359d4310
lisnote <didongxiaoli@gmail.com>
2023-09-20 16:05:10 +0800

    fix: dialog 路由不支持 Transition 组件

--- src/views/Components/Dialog.vue
+++ src/views/Components/Dialog.vue
@@ -16,26 +16,28 @@ const handleClose = (done: () => void) => {
 </script>
 
 <template>
-  <ElButton text @click="dialogVisible = true">
-    click to open the Dialog
-  </ElButton>
+  <div>
+    <ElButton text @click="dialogVisible = true">
+      click to open the Dialog
+    </ElButton>
 
-  <ElDialog
-    v-model="dialogVisible"
-    title="Tips"
-    width="30%"
-    :before-close="handleClose"
-  >
-    <span>This is a message</span>
-    <template #footer>
-      <span class="dialog-footer">
-        <ElButton @click="dialogVisible = false">Cancel</ElButton>
-        <ElButton type="primary" @click="dialogVisible = false">
-          Confirm
-        </ElButton>
-      </span>
-    </template>
-  </ElDialog>
+    <ElDialog
+      v-model="dialogVisible"
+      title="Tips"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>This is a message</span>
+      <template #footer>
+        <span class="dialog-footer">
+          <ElButton @click="dialogVisible = false">Cancel</ElButton>
+          <ElButton type="primary" @click="dialogVisible = false">
+            Confirm
+          </ElButton>
+        </span>
+      </template>
+    </ElDialog>
+  </div>
 </template>
 
 <style scoped>
