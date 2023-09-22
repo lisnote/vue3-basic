d5a9cfb8c2a515c04672a594fc7d6d618a373b18
lisnote <didongxiaoli@gmail.com>
2023-09-22 14:13:54 +0800

    feat: mock 数据补充

--- mock/index.ts
+++ mock/index.ts
@@ -5,3 +5,9 @@ const baseUrl = '/backend';
 export function createMockMethod(...mockMethod: MockMethod[]): MockMethod[] {
   return mockMethod.map((item) => ({ ...item, url: baseUrl + item.url }));
 }
+
+export function queryList<T>(list: T[], page = 1, limit = 10) {
+  const end = page * limit;
+  const start = end - limit;
+  return list.slice(start, end);
+}
--- mock/user.ts
+++ mock/user.ts
@@ -1,5 +1,51 @@
-import { createMockMethod } from '.';
+import { createMockMethod, queryList } from '.';
 
+type Role = { id: string; name: string; children?: Role[] };
+const roleTree: Role[] = [
+  {
+    id: '0',
+    name: '总经理',
+    children: [
+      { id: '1', name: '实习经理' },
+      {
+        id: '2',
+        name: '技术总监',
+        children: [{ id: '3', name: '工程师' }],
+      },
+      {
+        id: '4',
+        name: '销售主管',
+        children: [{ id: '5', name: '销售员' }],
+      },
+      {
+        id: '6',
+        name: '产品经理',
+        children: [
+          { id: '7', name: '设计师' },
+          { id: '8', name: '调查员' },
+        ],
+      },
+    ],
+  },
+];
+const roleList = (function getRoleList(roleTree: Role[], list: Role[] = []) {
+  roleTree?.forEach((role) => {
+    list.push(role);
+    if (role.children?.length) getRoleList(role.children, list);
+  });
+  return list;
+})(roleTree);
+const userList = Array.from({ length: 96 }).map((_v, index) => {
+  const role = roleList[(Math.random() * roleList.length) | 0];
+  return {
+    id: String(++index),
+    name: `用户${index}`,
+    phone: String(18888888889 + index),
+    role: role.name,
+    roleId: role.id,
+    email: 18888888889 + index + '@lisnote.com',
+  };
+});
 export default createMockMethod(
   {
     url: '/user/login',
@@ -14,7 +60,7 @@ export default createMockMethod(
         return {
           code: 0,
           data: {
-            id: '123456',
+            id: '0',
             name: 'admin',
             phone: '18888888888',
             token: '123456',
@@ -41,5 +87,20 @@ export default createMockMethod(
   {
     url: '/user/logout',
     response: () => ({ code: 0 }),
+  },
+  {
+    url: '/user/getRoleTree',
+    response: () => ({
+      code: 0,
+      data: roleTree,
+    }),
+  },
+  {
+    url: '/user/getUserList',
+    response: ({ body: { page, limit } }) => ({
+      code: 0,
+      count: userList.length,
+      data: queryList(userList, page, limit),
+    }),
   },
 );
