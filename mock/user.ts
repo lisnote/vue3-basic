b0d25255ccc19ba8d8b3c8e1b6d4969c3ea6ae0d
lisnote <didongxiaoli@gmail.com>
2023-09-22 10:56:08 +0800

    perf: mock 简化

--- mock/user.ts
+++ mock/user.ts
@@ -3,36 +3,31 @@ import { createMockMethod } from '.';
 export default createMockMethod(
   {
     url: '/user/login',
-    rawResponse(this, req, res) {
-      res.setHeader('Content-Type', 'application/json');
-      this.parseJson().then(({ phone, password }) => {
-        if (phone != '18888888888') {
-          res.statusCode = 400;
-          res.end(JSON.stringify({ code: 40002 }));
-        } else if (password != '0192023a7bbd73250516f069df18b500') {
-          res.statusCode = 400;
-          res.end(JSON.stringify({ code: 40003 }));
-        } else {
-          res.end(
-            JSON.stringify({
-              code: 0,
-              data: {
-                id: '123456',
-                name: 'admin',
-                phone: '18888888888',
-                token: '123456',
-                avatar: 'https://avatars.githubusercontent.com/lisnote',
-                permissionList: [
-                  'permissionAdd',
-                  'permissionSelect',
-                  'permissionEdit',
-                  'permissionDelete',
-                ],
-              },
-            }),
-          );
-        }
-      });
+    response(this, { body: { phone, password } }) {
+      if (phone != '18888888888') {
+        this.res.statusCode = 400;
+        return { code: 40002 };
+      } else if (password != '0192023a7bbd73250516f069df18b500') {
+        this.res.statusCode = 400;
+        return { code: 40003 };
+      } else {
+        return {
+          code: 0,
+          data: {
+            id: '123456',
+            name: 'admin',
+            phone: '18888888888',
+            token: '123456',
+            avatar: 'https://avatars.githubusercontent.com/lisnote',
+            permissionList: [
+              'permissionAdd',
+              'permissionSelect',
+              'permissionEdit',
+              'permissionDelete',
+            ],
+          },
+        };
+      }
     },
   },
   {
