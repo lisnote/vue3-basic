b1e7f700e7481e1bfb6d4130a6220af6488dff60
lisnote <didongxiaoli@gmail.com>
2023-09-22 18:04:42 +0800

    feat: 完成部分职位与权限的布局

---
+++ src/views/System/RolePermission/PermissionTree.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>PermissionTree</div>
+</template>
--- src/views/System/RolePermission/RolePermission.vue
+++ src/views/System/RolePermission/RolePermission.vue
@@ -1,3 +1,30 @@
+<script setup lang="ts">
+import commonStyle from '@/styles/common.module.scss';
+import RoleTree from './RoleTree.vue';
+import PermissionTree from './PermissionTree.vue';
+</script>
+
 <template>
-  <div>RolePermission</div>
+  <div :class="commonStyle.contentArea" class="role-permission">
+    <div class="role-tree-container">
+      <RoleTree />
+    </div>
+    <div class="flex-1">
+      <PermissionTree />
+    </div>
+  </div>
 </template>
+<style lang="scss" scoped>
+.role-permission {
+  display: flex;
+
+  .role-tree-container {
+    // w-200px flex-shrink-0 border-r border-$el-border-color mr-10px
+    width: 200px;
+    flex: 0 0 200px;
+    border-right: 1px solid var(--el-border-color);
+    padding-right: 20px;
+    margin-right: 20px;
+  }
+}
+</style>
