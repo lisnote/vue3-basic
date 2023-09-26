19af4333594623a533e5953cbd71806986b04a15
lisnote <1292580280@qq.com>
2023-09-27 00:02:08 +0800

    feat: 完成基础的角色树组件

--- src/views/System/RolePermission/RoleTree.vue
+++ src/views/System/RolePermission/RoleTree.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import { getRoleTree } from '@/api/user';
+import { ElTree } from 'element-plus';
+import { ref } from 'vue';
+
+const emit = defineEmits<{ (e: 'node-click', id: string): void }>();
+
+const treeData = ref([]);
+getRoleTree().then(({ data: { data } }) => {
+  treeData.value = data;
+});
+
+function nodeClick({ id }: { id: string }) {
+  emit('node-click', id);
+}
+</script>
+
 <template>
-  <div>RoleTree</div>
+  <ElScrollbar>
+    <ElTree
+      :data="treeData"
+      :props="{ label: 'name' }"
+      default-expand-all
+      :expand-on-click-node="false"
+      draggable
+      @node-click="nodeClick($event)"
+    />
+  </ElScrollbar>
 </template>
