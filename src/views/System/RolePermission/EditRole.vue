ab2b23f2af097a0ddf0323e25a0eb9f3837d8778
lisnote <didongxiaoli@gmail.com>
2023-09-27 18:03:47 +0800

    feat: 完善部分 EditRole

--- src/views/System/RolePermission/EditRole.vue
+++ src/views/System/RolePermission/EditRole.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { type Role, updateRole, addRole } from '@/api/user';
 import { ElDialog } from 'element-plus';
-// import { ElDialog } from 'element-plus';
+import { ref } from 'vue';
 
 const props = withDefaults(
   defineProps<{
@@ -12,14 +12,22 @@ const props = withDefaults(
   { data: () => ({ id: '', name: '' }) },
 );
 const emit = defineEmits(['success', 'update:visible']);
+
+const formData = ref({ id: '', name: '' });
 </script>
 
 <template>
   <ElDialog
     :title="mode === 'add' ? '新增职位' : '编辑职位'"
+    width="300"
     :model-value="visible"
     @update:model-value="emit('update:visible', $event)"
   >
+    <ElForm>
+      <ElFormItem label="名称">
+        <ElInput v-model="formData.name" />
+      </ElFormItem>
+    </ElForm>
     <div v-for="(value, key) of props" :key="key">{{ key }} : {{ value }}</div>
   </ElDialog>
 </template>
