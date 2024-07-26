cfeb7d58864d987ce20f49f91ae142e149b3afdc
lisnote <lisnote@lisnote.com>
2024-07-26 21:04:21 +0800

    feat: 当element被移除时自动移除监听器

--- src/utils/event/resize.ts
+++ src/utils/event/resize.ts
@@ -1,14 +1,18 @@
 import { useRafThrottle } from '../functionOptimizers';
+import { addRemoveListener } from './remove';
 
 type ResizeListener = (event: {
   entries: ResizeObserverEntry[];
   observer: ResizeObserver;
 }) => void;
 
-const elementMap = new WeakMap<HTMLElement, ResizeListener[]>();
+const elementMap = new WeakMap<
+  HTMLElement,
+  { listeners: ResizeListener[]; observer: ResizeObserver }
+>();
 
 /**
- * 监听指定元素尺寸变化
+ * 监听元素的尺寸变化
  * @param element - 要添加监听器的元素
  * @param listener - 监听器
  */
@@ -18,10 +22,11 @@ export function addResizeListener(
 ) {
   const listeners = getMapValue(element);
   listeners.push(listener);
+  addRemoveListener(element, () => removeResizeListener(element, listener));
 }
 
 /**
- * 关闭元素的尺寸变化监听
+ * 移除元素的尺寸变化监听
  * @param element - 要移除监听器的元素
  * @param listener - 要移除的监听器
  */
@@ -29,17 +34,21 @@ export function removeResizeListener(
   element: HTMLElement,
   listener: ResizeListener,
 ) {
-  const listeners = elementMap.get(element);
-  if (listeners) listeners.splice(listeners.indexOf(listener), 1);
+  const { listeners, observer } = elementMap.get(element) ?? {};
+  if (!listeners?.length) return;
+  listeners.splice(listeners.indexOf(listener), 1);
+  if (!listener.length) observer?.disconnect();
 }
 
 function getMapValue(element: HTMLElement) {
-  let listeners = elementMap.get(element);
-  if (listeners) return listeners;
+  let { listeners } = elementMap.get(element) ?? {};
+  if (listeners?.length) return listeners;
   listeners = [];
-  const ro = new ResizeObserver(useRafThrottle(resizeHandle.bind(listeners)));
-  ro.observe(element);
-  elementMap.set(element, listeners);
+  const observer = new ResizeObserver(
+    useRafThrottle(resizeHandle.bind(listeners)),
+  );
+  observer.observe(element);
+  elementMap.set(element, { listeners, observer });
   return listeners;
 }
 
